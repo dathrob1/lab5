1ea1ba926a2af78f78237a247898012436066b24
misganaw meseret <it.misganaw.meseret1@gmail.com>
2021-01-31 16:36:50 +0300

    work on bom file

--- assets/js/bom.js
+++ assets/js/bom.js
@@ -14,19 +14,118 @@ Adding Extra is Possible if you want to explore more ...
 Good Luck !!! 
 */
 
-var href = location.href;
-var protocol = location.protocol;
-var host = location.host;
-var port = location.port;
-var hostname = location.hostname;
-var appname = navigator.appName;
-var appversion = navigator.appVersion;
-var platform = navigator.platform;
-var language = navigator.language;
-var cookieenabled = navigator.cookieEnabled;
-var height = screen.availHeight;
-var width = screen.width;
-var pixeldepth = screen.pixelDepth;
+
+
+ 
+const href = document.querySelector('#href')
+const protocol = document.querySelector('#protocol')
+const host = document.querySelector('#host')
+const port = document.querySelector('#port')
+const hostname = document.querySelector('#hostname')
+const appname = document.querySelector('#appname')
+const appversion = document.querySelector('#appversion')
+const platform = document.querySelector('#platform')
+const language = document.querySelector('#language')
+const cookieenabled = document.querySelector('#cookieenabled')
+const height = document.querySelector('#height')
+const width = document.querySelector('#width')
+const pixeldepth = document.querySelector('#pixeldepth')
+const length = document.querySelector('#length')
+const state = document.querySelector('#state')
+href.addEventListener('click', hreffinder);
+protocol.addEventListener('click', protocolfinder);
+host.addEventListener('click', hostfinder);
+port.addEventListener('click', portfinder);
+hostname.addEventListener('click',hostnamefinder)
+appname.addEventListener('click', appnamefinder);
+appversion.addEventListener('click', appversionfinder);
+platform.addEventListener('click', platformfinder);
+language.addEventListener('click', languagefinder);
+cookieenabled.addEventListener('click', cookieenabledfinder);
+height.addEventListener('click', heightfinder);
+width.addEventListener('click', widthfinder);
+pixeldepth.addEventListener('click', pixeldepthfinder);
+length.addEventListener('click', lengthfinder);
+state.addEventListener('click', statefinder);
+
+
+
+function hreffinder(){
+
+    document.getElementById("href_answer").innerHTML = location.href;
+
+}
+function protocolfinder(){
+
+    document.getElementById("protocol_answer").innerHTML = location.protocol;
+
+}
+function hostfinder(){
+
+    document.getElementById("host_answer").innerHTML = location.host;
+
+}
+function portfinder(){
+
+    document.getElementById("port_answer").innerHTML = location.port;
+
+}
+function hostnamefinder(){
+
+    document.getElementById("hostname_answer").innerHTML = location.hostname;
+
+}
+function appnamefinder(){
+
+    document.getElementById("appname_answer").innerHTML = navigator.appname;
+
+}
+function appversionfinder(){
+
+    document.getElementById("appversion_answer").innerHTML = navigator.appVersion;
+
+}
+function platformfinder(){
+
+    document.getElementById("platform_answer").innerHTML = navigator.platform;
+
+}
+function languagefinder(){
+
+    document.getElementById("language_answer").innerHTML = navigator.language;
+
+}
+function cookieenabledfinder(){
+
+    document.getElementById("cookieenabled_answer").innerHTML = navigator.cookieEnabled;
+
+}
+function heightfinder(){
+
+    document.getElementById("height_answer").innerHTML = screen.height;
+
+}
+function widthfinder(){
+
+    document.getElementById("width_answer").innerHTML = screen.width;
+
+}
+function pixeldepthfinder(){
+
+    document.getElementById("pixel_answer").innerHTML = screen.pixelDepth;
+
+}
+function lengthfinder(){
+
+    document.getElementById("length_answer").innerHTML = history.length;
+
+}
+function statefinder(){
+
+    document.getElementById("state_answer").innerHTML = history.state;
+
+}
+
 
 
 
