369b291eee4ecde8ae751278d7d4d784a430d4fe
misganaw meseret <it.misganaw.meseret1@gmail.com>
2021-01-31 14:37:39 +0300

    fix bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,22 @@
 // Define UI Variables 
-const taskInput = document.querySelector('#task'); //the task input text field
-const form = document.querySelector('#task-form'); //The form at the top
-const filter = document.querySelector('#filter'); //the task filter text field
-const taskList = document.querySelector('.collection'); //The UL
-const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
+const taskInput = document.querySelector('#task'); 
+const form = document.querySelector('#task-form');
+const filter = document.querySelector('#filter'); 
+const taskList = document.querySelector('.collection'); 
+const clearBtn = document.querySelector('.clear-tasks'); 
 
-const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
+const reloadIcon = document.querySelector('.fa'); 
 
-// Add Event Listener  [Form , clearBtn and filter search input ]
 
-// form submit 
+
 form.addEventListener('submit', addNewTask);
-// Clear All Tasks
+
 clearBtn.addEventListener('click', clearAllTasks);
-//   Filter Task 
+
 filter.addEventListener('keyup', filterTasks);
-// Remove task event [event delegation]
+
 taskList.addEventListener('click', removeTask);
-// Event Listener for reload 
+ 
 reloadIcon.addEventListener('click', reloadPage);
 
 
